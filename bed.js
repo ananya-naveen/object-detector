43d95fbe1c7784086f60a11c61bf279fe09314a2
ananya-naveen <84455616+ananya-naveen@users.noreply.github.com>
2022-02-21 19:02:00 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,45 @@
+img="";
+status="";
+objects=[];
+
+function preload(){
+    img=loadImage("bed.jpeg");
+}
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+
+function draw(){
+    image(img,0,0,640,420);
+    if(status!=""){
+        for(z=0;z<objects.length;z++){
+            document.getElementById("status").innerHTML="Status: Objects Detected";
+            fill("#800000");
+            percent=floor(objects[z].confidence*100);
+            text(objects[z].label+" "+percent+"%",objects[z].x+15,objects[z].y+15);
+            noFill();
+            stroke("#800000");
+            rect(objects[z].x,objects[z].y,objects[z].width,objects[z].height);
+        }
+    }
+}
